4efcd32712d439925619991a7b83a95d3dd0090f
baba-s <shotababa1206@gmail.com>
2022-08-27 09:48:20 +0900

    add

---
+++ Editor/CopyUnityVersionMenuItem.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+    internal static class CopyUnityVersionMenuItem
+    {
+        [MenuItem( "Kogane/Unity バージョン/クリップボードにコピー" )]
+        public static void Copy()
+        {
+            var unityVersion = Application.unityVersion;
+            EditorGUIUtility.systemCopyBuffer = unityVersion;
+
+            EditorUtility.DisplayDialog
+            (
+                title: "",
+                message: $"Unity のバージョンをクリップボードにコピーしました\n{unityVersion}",
+                ok: "ok"
+            );
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.CopyUnityVersionMenuItem
+# Kogane Copy Unity Version Menu Item
+
+使用中の Unity のバージョンをコピーできる MenuItem を追加するエディタ拡張
